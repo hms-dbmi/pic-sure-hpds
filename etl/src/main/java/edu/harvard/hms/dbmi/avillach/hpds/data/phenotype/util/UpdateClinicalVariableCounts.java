58b472a42399e4c1fc701fd7fbad568047dad2bb
sharra <sharra@MBI-STED-L06787.hsd1.ma.comcast.net>
2023-02-24 13:52:27 -0500

    Clinical Variable Counts Update

--- etl/src/main/java/edu/harvard/hms/dbmi/avillach/hpds/data/phenotype/util/UpdateClinicalVariableCounts.java
+++ etl/src/main/java/edu/harvard/hms/dbmi/avillach/hpds/data/phenotype/util/UpdateClinicalVariableCounts.java
@@ -10,6 +10,7 @@ import java.io.ObjectOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collection;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
@@ -19,7 +20,6 @@ import java.util.zip.GZIPOutputStream;
 import edu.harvard.hms.dbmi.avillach.hpds.data.phenotype.ColumnMeta;
 public class UpdateClinicalVariableCounts {
     protected static final String COLUMN_META_FILE = "/opt/local/hpds/columnMeta.javabin";
-    protected static Set<Integer> allIds;
 
     public static void main(String[] args) throws ClassNotFoundException, FileNotFoundException, IOException {
 	
@@ -27,32 +27,39 @@ public class UpdateClinicalVariableCounts {
 			throw new RuntimeException("Column Metadata file - " + COLUMN_META_FILE  + " -  does not exist!");
 		}			
 
-		TreeMap<String, ColumnMeta> metadata = updateMetadata();
+		TreeMap<String, Integer> counts = updateCounts();
 			
 		ObjectOutputStream metaOut = new ObjectOutputStream(new GZIPOutputStream(new FileOutputStream(new File(COLUMN_META_FILE))));
 			
-		metaOut.writeObject(metadata); 
-		
-		metaOut.writeObject(allIds); 
+		metaOut.writeObject(counts); 
 		metaOut.flush();
 		metaOut.close();
 	}
 
-    protected static TreeMap<String, ColumnMeta> updateMetadata(){
+    protected static TreeMap<String, Integer> updateCounts(){
+        TreeMap<String, Integer> counts = new TreeMap<String, Integer>();
         try (ObjectInputStream objectInputStream = new ObjectInputStream(new GZIPInputStream(new FileInputStream(COLUMN_META_FILE)))){
 		
 			TreeMap<String, ColumnMeta> metastore = (TreeMap<String, ColumnMeta>) objectInputStream.readObject();
+			Collection<ColumnMeta> columnMetas = metastore.values();
+            columnMetas.forEach(value -> {
+                String studyId = value.getName();
+                if(counts.containsKey(studyId)){
+                    counts.replace(studyId, counts.get(studyId)+1);
+                }
+                else{
+                   counts.put(studyId, 1);     
+                }
+            });
 			
-			allIds = (TreeSet<Integer>) objectInputStream.readObject();
+						
 			
-			System.out.println("allIds size = " + allIds.size());			
 			
-			return metastore;
 		} catch (IOException | ClassNotFoundException e) {
 			e.printStackTrace();
 			throw new RuntimeException("Could not load metastore");
 		} 
-
+        return counts;
 
     }
 }
