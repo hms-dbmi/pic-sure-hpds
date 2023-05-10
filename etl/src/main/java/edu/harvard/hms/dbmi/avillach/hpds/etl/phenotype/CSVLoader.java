3a1b95738de09a64467ed2b33e89a3a8bdf43ea4
Ryan Amari <ramari16@gmail.com>
2023-05-10 10:29:38 -0400

    ALS-4465: Parameterize hpds data directory

--- etl/src/main/java/edu/harvard/hms/dbmi/avillach/hpds/etl/phenotype/CSVLoader.java
+++ etl/src/main/java/edu/harvard/hms/dbmi/avillach/hpds/etl/phenotype/CSVLoader.java
@@ -31,15 +31,23 @@ public class CSVLoader {
 
 	private static final int DATETIME = 4;
 
+	private static String HPDS_DIRECTORY = "/opt/local/hpds/";
+
 	public static void main(String[] args) throws IOException {
-		store.allObservationsStore = new RandomAccessFile("/opt/local/hpds/allObservationsStore.javabin", "rw");
+		if (args.length > 0) {
+			HPDS_DIRECTORY = args[1];
+			if (!HPDS_DIRECTORY.endsWith("/")) {
+				HPDS_DIRECTORY += "/";
+			}
+		}
+		store.allObservationsStore = new RandomAccessFile(HPDS_DIRECTORY + "allObservationsStore.javabin", "rw");
 		initialLoad();
 		store.saveStore();
 	}
 
 	private static void initialLoad() throws IOException {
 		Crypto.loadDefaultKey();
-		Reader in = new FileReader("/opt/local/hpds/allConcepts.csv");
+		Reader in = new FileReader(HPDS_DIRECTORY + "allConcepts.csv");
 		Iterable<CSVRecord> records = CSVFormat.DEFAULT.withSkipHeaderRecord().withFirstRecordAsHeader().parse(new BufferedReader(in, 1024*1024));
 
 		final PhenoCube[] currentConcept = new PhenoCube[1];
