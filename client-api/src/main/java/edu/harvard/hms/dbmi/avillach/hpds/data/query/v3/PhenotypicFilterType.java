da2d3dcc78f24b122a7dfc638f27db5828281ce8
Ryan Amari <ramari16@gmail.com>
2026-02-12 18:18:08 +0100

    ALS-9545: Add setter for Query authorization filters

--- client-api/src/main/java/edu/harvard/hms/dbmi/avillach/hpds/data/query/v3/PhenotypicFilterType.java
+++ client-api/src/main/java/edu/harvard/hms/dbmi/avillach/hpds/data/query/v3/PhenotypicFilterType.java
@@ -7,8 +7,8 @@ public enum PhenotypicFilterType {
     REQUIRED, @Schema(
         description = "Specifies that a filter will match if a concept path matches the values specified in the `PhenotypicFilter`"
     )
-    FILTER, @Schema(
-        description = "Specifies that a filter will match if the concept path contains any value for itself or any child concept paths"
-    )
+    FILTER,
+    // todo: explain this more specifically
+    @Schema(description = "Specifies that a filter will match if the concept path contains any value for itself or any child concept paths")
     ANY_RECORD_OF
 }
