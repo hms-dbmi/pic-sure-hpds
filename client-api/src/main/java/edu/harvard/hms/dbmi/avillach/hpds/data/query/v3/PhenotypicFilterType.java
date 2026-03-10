592d54f6b67b9cfc36c19abef05a2fd6a8ec6b5b
Ryan Amari <ramari16@gmail.com>
2026-03-10 14:51:51 -0400

    ALS-9545: Remove obsolete todo

--- client-api/src/main/java/edu/harvard/hms/dbmi/avillach/hpds/data/query/v3/PhenotypicFilterType.java
+++ client-api/src/main/java/edu/harvard/hms/dbmi/avillach/hpds/data/query/v3/PhenotypicFilterType.java
@@ -7,8 +7,8 @@ public enum PhenotypicFilterType {
     REQUIRED, @Schema(
         description = "Specifies that a filter will match if a concept path matches the values specified in the `PhenotypicFilter`"
     )
-    FILTER,
-    // todo: explain this more specifically
-    @Schema(description = "Specifies that a filter will match if the concept path contains any value for itself or any child concept paths")
+    FILTER, @Schema(
+        description = "Specifies that a filter will match if the concept path contains any value for itself or any child concept paths"
+    )
     ANY_RECORD_OF
 }
