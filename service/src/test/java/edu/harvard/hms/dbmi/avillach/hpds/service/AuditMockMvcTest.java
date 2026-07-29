ae8ff0934e59608f3d1b35f57449f4a83abc33ae
Ryan Amari <ramari16@gmail.com>
2026-07-29 14:09:20 -0400

    Attempt to fix tests running in github actions

--- service/src/test/java/edu/harvard/hms/dbmi/avillach/hpds/service/AuditMockMvcTest.java
+++ service/src/test/java/edu/harvard/hms/dbmi/avillach/hpds/service/AuditMockMvcTest.java
@@ -16,7 +16,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -37,7 +36,7 @@ class AuditMockMvcTest {
     @Autowired
     MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     LoggingClient loggingClient;
 
     @MockitoBean
